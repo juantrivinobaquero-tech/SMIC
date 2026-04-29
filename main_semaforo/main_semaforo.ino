78beb6bd958d639af9c235087aa7504be3e952b1
Juanblue11 <juantrivinobaquero@gmail.com>
2026-04-28 21:39:35 -0500

    Cambio 2

--- main_semaforo/main_semaforo.ino
+++ main_semaforo/main_semaforo.ino
@@ -1,8 +1,9 @@
+//Conexion a WIFI (pendiente de terminar)
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-const char* ssid = "sc-17f0";
-const char* password = "Q5DN24V4FLYG";
+const char* ssid = "";
+const char* password = "";
 
 const char* mqtt_server = "broker.hivemq.com";
 
@@ -10,7 +11,7 @@ WiFiClient   espClient;
 PubSubClient client(espClient);
 
 
-
+//Definir los pines de la ESP (pendiente) ------------------------------------
 #define norter 5
 #define nortea 5
 #define nortev 5
@@ -28,7 +29,6 @@ PubSubClient client(espClient);
 #define oestev 5
 
 
-
 #define norte_ir_ade 5
 #define norte_ir_atr 5
 
@@ -41,7 +41,7 @@ PubSubClient client(espClient);
 #define oeste_ir_ade 5
 #define oeste_ir_atr 5
 
-
+// Definiendo los semáforos --------------------------
 
 int SEM_Norte[3] = {norter, nortea, nortev};
 
@@ -64,6 +64,16 @@ bool oeste_atr_ante = 1;
 bool norte_atr_ante = 1;
 bool sur_atr_ante = 1;
 
+long tiempo_conteo_este = 0;
+long tiempo_conteo_oeste = 0;
+long tiempo_conteo_norte = 0;
+long tiempo_conteo_sur = 0;
+
+bool flag_norte_c = 0;
+bool flag_sur_c = 0;
+bool flag_este_c = 0;
+bool flag_oeste_c = 0;
+
 // Hay carros esperando? variables-------------------
 bool espera_norte_sur = false;
 bool espera_este_oeste = false;
@@ -84,8 +94,11 @@ bool este_ante = 0;
 bool oeste_ante = 0;
 //------------------------------------------
 
-bool hayAutos_n_s() {
 
+//Funciones para saber si hay carros esperando, nos sirven las dos para las que se enfrentan.------------------------
+
+
+bool hayAutos_n_s() {
 //NORTE_______________
   if((norte_ante == HIGH) && (digitalRead(norte_ir_ade) == LOW)){
     tiempo_espera_norte = millis();
@@ -115,8 +128,6 @@ bool hayAutos_n_s() {
   sur_ante   = (digitalRead(sur_ir_ade));
   return false;
 }
-
-
 
 
 bool hayAutos_e_o(){
@@ -152,15 +163,70 @@ bool hayAutos_e_o(){
 }
 
 
-void contarautos_este(){
-
-  if ((este_atr_ante == HIGH) && (digitalRead(este_ir_atr) == LOW)) {
-    pasaron_este = pasaron_este + 1;
+//funciones para contar los carros que van pasando en cada calle.---------------------------------------
+//NORTE--------------------------------------------
+void contar_autos_norte(){
+  if ((norte_atr_ante == HIGH) && (digitalRead(norte_ir_atr) == LOW)) {
+    tiempo_conteo_norte = millis();
+    flag_norte_c = 1;
   }
-
-  este_atr_ante = (digitalRead(este_ir_atr));
+  if((norte_atr_ante == LOW) && (digitalRead(norte_ir_atr) == HIGH)){
+    flag_norte_c = 0;
+  }
+  if(((millis() - tiempo_conteo_norte) > 150) && (flag_norte_c == 1)){
+    flag_norte_c = 0;
+    pasaron_norte = pasaron_norte + 1;
+  }
+  norte_atr_ante = digitalRead(norte_ir_atr);
 }
 
+//-------------------------SUR--------------------------
+void contar_autos_sur(){
+  if ((sur_atr_ante == HIGH) && (digitalRead(sur_ir_atr) == LOW)) {
+    tiempo_conteo_sur = millis();
+    flag_sur_c = 1;
+  }
+  if((sur_atr_ante == LOW) && (digitalRead(sur_ir_atr) == HIGH)){
+    flag_sur_c = 0;
+  }
+  if(((millis() - tiempo_conteo_sur) > 150) && (flag_sur_c == 1)){
+    flag_sur_c = 0;
+    pasaron_sur = pasaron_sur + 1;
+  }
+  sur_atr_ante = digitalRead(sur_ir_atr);
+}
+
+//-------------------------ESTE--------------------------
+void contar_autos_este(){
+  if ((este_atr_ante == HIGH) && (digitalRead(este_ir_atr) == LOW)) {
+    tiempo_conteo_este = millis();
+    flag_este_c = 1;
+  }
+  if((este_atr_ante == LOW) && (digitalRead(este_ir_atr) == HIGH)){
+    flag_este_c = 0;
+  }
+  if(((millis() - tiempo_conteo_este) > 150) && (flag_este_c == 1)){
+    flag_este_c = 0;
+    pasaron_este = pasaron_este + 1;
+  }
+  este_atr_ante = digitalRead(este_ir_atr);
+}
+
+//-------------------------OESTE--------------------------
+void contar_autos_oeste(){
+  if ((oeste_atr_ante == HIGH) && (digitalRead(oeste_ir_atr) == LOW)) {
+    tiempo_conteo_oeste = millis();
+    flag_oeste_c = 1;
+  }
+  if((oeste_atr_ante == LOW) && (digitalRead(oeste_ir_atr) == HIGH)){
+    flag_oeste_c = 0;
+  }
+  if(((millis() - tiempo_conteo_oeste) > 150) && (flag_oeste_c == 1)){
+    flag_oeste_c = 0;
+    pasaron_oeste = pasaron_oeste + 1;
+  }
+  oeste_atr_ante = digitalRead(oeste_ir_ade);
+}
 
 
 
